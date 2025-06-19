aeecaa9d7e1c878dfd3d859a40cce87c465c65c6
Ghost322wwww <kwokc289@yahoo.com>
2025-06-20 01:20:41 +0800

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],  
+  verbose: true,                        
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  }
+};
