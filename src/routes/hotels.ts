76fd2ecad4e5bdea8fa331f413e77d5fa886217c
Ghost322wwww <kwokc289@yahoo.com>
2025-06-20 23:51:36 +0800

    Update hotels.ts

--- src/routes/hotels.ts
+++ src/routes/hotels.ts
@@ -1,19 +1,20 @@
 import express from 'express';
 import {
   getHotels,
-  getHotelById,
   addHotel,
+  getHotelById,
   updateHotel,
   deleteHotel
 } from '../controllers/hotelController';
+
 import { authenticate } from '../middleware/authMiddleware';
 import { authorizeRole } from '../middleware/authorize';
 
 const router = express.Router();
 
 router.get('/', getHotels);
-router.get('/:id', getHotelById);
 router.post('/', authenticate, authorizeRole('operator'), addHotel);
+router.get('/:id', getHotelById);
 router.put('/:id', authenticate, authorizeRole('operator'), updateHotel);
 router.delete('/:id', authenticate, authorizeRole('operator'), deleteHotel);
 
